8608a6f4c8ff2e6a020f8bc60b56451ddf2d4d5f
Ricardo Boss <ricardo.boss@web.de>
2016-07-06 13:59:04 +0200

    dang it

--- src/Results.php
+++ src/Results.php
@@ -10,25 +10,25 @@ namespace LoginLib;
  * An abstract class that is used to provide results of methods
  */
 abstract class MethodResult {
-	const UNDEFINED = -1;
-
+	const UNDEFINED = - 1;
+	
 	/** @var int Contains the method result */
 	private $result = MethodResult::UNDEFINED;
-
+	
 	/**
 	 * A constructor for LoginResults
-	 * 
+	 *
 	 * @param int $result The result of the login function, has to be one of the constants of this class
-	 * 
+	 *
 	 * @return LoginResult
 	 */
 	public function __construct($result) {
 		$this->result = $result;
 	}
-
+	
 	/**
 	 * Returns the result of this LoginResult
-	 * 
+	 *
 	 * @return int
 	 */
 	public function getResult() {
@@ -37,7 +37,7 @@ abstract class MethodResult {
 	
 	/**
 	 * Returns a simple result in form of a boolean value
-	 * 
+	 *
 	 * @return bool
 	 */
 	public abstract function getSimpleResult();
@@ -47,11 +47,10 @@ abstract class MethodResult {
  * This class is for results of the login method
  */
 class LoginResult extends MethodResult {
-	const USERNAME_NOT_FOUND =	0;
-	const PASSWORD_WRONG =		1;
-	const SUCCESS =				2;
-
-
+	const USERNAME_NOT_FOUND = 0;
+	const PASSWORD_WRONG = 1;
+	const SUCCESS = 2;
+	
 	/**
 	 * Returns a simple result in form of a boolean value
 	 *
@@ -59,10 +58,10 @@ class LoginResult extends MethodResult {
 	 */
 	public function getSimpleResult() {
 		switch ($this->result) {
-			case SUCCESS:
+			case SUCCESS :
 				return true;
-
-			default:
+			
+			default :
 				return false;
 		}
 	}
@@ -72,12 +71,11 @@ class LoginResult extends MethodResult {
  * This class is for results of the register method
  */
 class RegisterResult extends MethodResult {
-	const USERNAME_GIVEN =		0;
-	const EMAIL_GIVEN =			1;
-	const PASSWORD_MITMATCH =	2;
-	const SUCCESS =				3;
-
-
+	const USERNAME_GIVEN = 0;
+	const EMAIL_GIVEN = 1;
+	const PASSWORD_MITMATCH = 2;
+	const SUCCESS = 3;
+	
 	/**
 	 * Returns a simple result in form of a boolean value
 	 *
@@ -85,10 +83,10 @@ class RegisterResult extends MethodResult {
 	 */
 	public function getSimpleResult() {
 		switch ($this->result) {
-			case SUCCESS:
+			case SUCCESS :
 				return true;
 			
-			default:
+			default :
 				return false;
 		}
 	}
