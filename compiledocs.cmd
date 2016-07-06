721ad9df797fa5175d2368f2bd370e98d6cbad86
Ricardo Boss <ricardo.boss@web.de>
2016-07-06 13:33:22 +0200

    moved out of official repo

--- compiledocs.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-php phpDocumentor.phar -d "E:\XAMPP\htdocs\projects\LoginLib\src" -t "E:\XAMPP\htdocs\projects\LoginLib\docs" -f "E:\XAMPP\htdocs\projects\LoginLib\deps\MysqliDb.php"
-pause
