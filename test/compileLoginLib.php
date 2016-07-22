2a89c6209af529d11465fda230fc686d43aa758d
Ricardo Boss <ricardo.boss@web.de>
2016-07-22 02:02:28 +0200

    fixed compiler (hopefully)

--- test/compileLoginLib.php
+++ test/compileLoginLib.php
@@ -1,7 +1,7 @@
 <?php
 class Compiler {
-	const root = dirname(__DIR__);
-	const outputfile = Compiler::root.'dist/LoginLib.php';
+	public $root;
+	public $outputfile;
 	const sourcefiles = array(
 		'LoginLib.php',
 		'Config.php',
@@ -10,33 +10,43 @@ class Compiler {
 		'User.php',
 		'Exceptions.php'
 	);
+	
+	public function __construct($root) {
+		$this->root = $root;
+		$this->outputfile = $this->root.'/dist/LoginLib.php';
+	}
 
-	public static function start() {
+	public function compile() {
 		$starttime = microtime(true);
 
-		file_put_contents(Compiler::outputfile, "<?php\r\n");
+		file_put_contents($this->outputfile, "<?php\r\n");
 
-		Compiler::append("/**\r\n * This file contains all classes for LoginLib.\r\n * \r\n * Dont't forget the dependency on MysqliDb!\r\n */\r\n");
-		Compiler::append("namespace LoginLib;");
+		// add file comment
+		$this->append("/**\r\n * This file contains all classes for LoginLib.\r\n * \r\n * Dont't forget the dependency on MysqliDb!\r\n */\r\n");
+		
+		// add namespace
+		$this->append("namespace LoginLib;");
 
+		// loop through the source files and add them to the output file
 		foreach (Compiler::sourcefiles as $file) {
 			if (substr($file, 0, 1) !== ".")
-				Compiler::append(file_get_contents(Compiler::root.'src'.DIRECTORY_SEPARATOR.$file));
+				$this->append(file_get_contents($this->root.'/src/'.$file));
 		}
 
 		echo "Finished! (Took: ".(microtime(true) - $starttime)."ms)";
 	}
 
-	private static function append($file) {
-		// filter the file description and the php tag away...
+	private function append($file) {
+		// filter the file description and the php tag + namespaces away...
 		$file = preg_replace("/\\<\\?php.*(use [a-z\\\]*;|namespace [a-z\\\]*;)/is", "", $file);
 
 		// just append the file to the final one
-		file_put_contents(Compiler::outputfile, $file, FILE_APPEND | LOCK_EX);
+		file_put_contents($this->outputfile, $file, FILE_APPEND | LOCK_EX);
 	}
 }
 
-Compiler::start();
+$c = new Compiler(dirname(__DIR__));
+$c->compile();
 
 // Test if no php errors are thrown
-require(Compiler::outputfile);
+require($c->outputfile);
