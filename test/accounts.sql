27c9a80b49e101416995e74a8df420925e68c083
Ricardo Boss <ricardo.boss@web.de>
2016-07-25 01:28:29 +0200

    updated sql queries

--- test/accounts.sql
+++ test/accounts.sql
@@ -1,25 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jul 22, 2016 at 03:12 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
---
--- Database: `loginlib`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `accounts`
---
--- Creation: Jul 06, 2016 at 04:12 AM
---
 
 CREATE TABLE `accounts` (
   `id` bigint(255) UNSIGNED NOT NULL,
@@ -28,26 +7,12 @@ CREATE TABLE `accounts` (
   `password_hash` varchar(255) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `registered_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `accounts`
---
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `accounts`
---
 ALTER TABLE `accounts`
   MODIFY `id` bigint(255) UNSIGNED NOT NULL AUTO_INCREMENT;
--- test/login_tokens.sql
+++ test/login_tokens.sql
@@ -1,25 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jul 22, 2016 at 03:13 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
---
--- Database: `loginlib`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login_tokens`
---
--- Creation: Jul 15, 2016 at 08:54 PM
---
 
 CREATE TABLE `login_tokens` (
   `id` bigint(255) UNSIGNED NOT NULL,
@@ -27,24 +6,10 @@ CREATE TABLE `login_tokens` (
   `token` varchar(64) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `logged_out` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `login_tokens`
---
 ALTER TABLE `login_tokens`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `login_tokens`
---
 ALTER TABLE `login_tokens`
   MODIFY `id` bigint(255) UNSIGNED NOT NULL AUTO_INCREMENT;
