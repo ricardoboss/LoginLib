33111879a803b2bd239bb44766103aef96bc109d
Ricardo Boss <ricardo.boss@web.de>
2016-08-15 18:48:22 +0200

    updated project

--- test/loggedin.php
+++ test/loggedin.php
@@ -10,13 +10,11 @@ require('load.php');
 <body>
 	<h1>LoginLib Test Page</h1>
 	<div>
-		<p>
-			<form action="index.php" method="post">
-				You are <span class="<?php echo $loginlib->isLoggedIn() ? 'text-success">':'text-danger">not '; ?>logged in</span>!<br>
-				<button type="submit">Log<?php echo $loginlib->isLoggedIn() ? 'out':'in'; ?></button>
-				<input type="hidden" name="method" value="<?php echo $loginlib->isLoggedIn() ? 'logout':'redir'; ?>">
-			</form>
-		</p>
+		<form action="index.php" method="post">
+			You are <span class="<?php echo $loginlib->isLoggedIn() ? 'text-success">':'text-danger">not '; ?>logged in</span>!<br>
+			<button type="submit">Log<?php echo $loginlib->isLoggedIn() ? 'out':'in'; ?></button>
+			<input type="hidden" name="method" value="<?php echo $loginlib->isLoggedIn() ? 'logout':'redir'; ?>">
+		</form>
 		<br>
 		<pre><?php unset($_SERVER); var_dump($GLOBALS); ?></pre>
 	</div>
