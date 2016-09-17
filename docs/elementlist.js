a5693e7f7966ddc88f61d7e9058c99dc3e72a279
Ricardo Boss <ricardo.boss@web.de>
2016-09-18 01:08:32 +0200

    updated docs

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","LoginLib\\Config"],["c","LoginLib\\Exceptions\\ConfigurationException"],["c","LoginLib\\Exceptions\\DatabaseException"],["c","LoginLib\\IDatabase"],["c","LoginLib\\LoginLib"],["c","LoginLib\\Results\\LoginResult"],["c","LoginLib\\Results\\MethodResult"],["c","LoginLib\\Results\\RegisterResult"],["c","LoginLib\\User"]];
